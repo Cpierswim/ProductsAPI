5509a970cef388490ee905b027cb8e041e152c0f
Chris Pierson <cpierswim@gmail.com>
2023-03-10 15:23:27 -0500

    NEW: Added Get All Cars to API

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow import post_load, fields, ValidationError;
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 from dotenv import load_dotenv
@@ -35,11 +36,31 @@ class Product(db.Model):
         return f'ID: {self.id} Name: {self.name} {self.discription} {self.price} {self.inventory_quantity} {self.img_url}'
 
 # Schemas
+class ProductSchema(ma.Schema):
+    id = fields.Integer(primary_key=True)
+    name = fields.String(required=True)
+    discription = fields.String(required=True)
+    price = fields.Float(required=True)
+    inventory_quantity = fields.Integer(required=True)
+    img_url = fields.String()
 
+    class Meta:
+        fields = ('id', 'name', 'discription', 'price', 'inventory_quantity', 'img_url')
+
+    @post_load
+    def create_product(self, data, **kwargs):
+        return Product(**data)
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
 
 # Resources
-
+class ProductListResoucre(Resource):
+    def get():
+        all_produts = Product.query.all()
+        return all_produts
 
 
 # Routes
+api.add_resource(ProductListResoucre, '/api/products')
